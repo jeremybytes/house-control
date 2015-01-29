91b7c8326ddfd20945d2f57ec30f068bd86683a4
Jeremy Clark <jeremy@jeremybytes.com>
2015-01-29 08:01:40 -0800

    More test name updates

--- HouseControl.Sunset.Test/SunriseSunsetOrgTest.cs
+++ HouseControl.Sunset.Test/SunriseSunsetOrgTest.cs
@@ -7,7 +7,7 @@ namespace HouseControl.Sunset.Test
     public class SunriseSunsetOrgTest
     {
         [TestMethod]
-        public void UTCTimeString_WithValidValue_IsValid()
+        public void UTCTimeString_WithValidValue_HasValue()
         {
             string value = "01:20:30 AM";
             var timeString = new UTCTimeString(value);
@@ -31,7 +31,7 @@ namespace HouseControl.Sunset.Test
         }
 
         [TestMethod]
-        public void ResponseContentString_WithJSONValue_IsValid()
+        public void ResponseContentString_WithJSONValue_HasValue()
         {
             string value = "{\"results\":{\"sunrise\":\"2:56:10 PM\",\"sunset\":\"1:06:09 AM\",\"solar_noon\":\"8:01:09 PM\",\"day_length\":\"10:09:59\",\"civil_twilight_begin\":\"2:29:14 PM\",\"civil_twilight_end\":\"1:33:05 AM\",\"nautical_twilight_begin\":\"1:58:37 PM\",\"nautical_twilight_end\":\"2:03:42 AM\",\"astronomical_twilight_begin\":\"1:28:38 PM\",\"astronomical_twilight_end\":\"2:33:41 AM\"},\"status\":\"OK\"}";
             var contentString = new ResponseContentString(value);
