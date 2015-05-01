321fdf1ae389f0d1bf4b28a8c23505b801a9e72c
Jeremy Clark <jeremy@jeremybytes.com>
2015-05-01 15:17:28 -0700

    Initial project for schedule editor

---
+++ HouseControl.Editor/EditorView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HouseControl.Editor
+{
+    public partial class EditorView : UserControl
+    {
+        private EditorViewModel viewModel;
+
+        public EditorView()
+        {
+            InitializeComponent();
+            viewModel = new EditorViewModel();
+            DataContext = viewModel;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.SaveSchedule();
+        }
+    }
+}
---
+++ HouseControl.Editor/EditorViewModel.cs
@@ -0,0 +1,54 @@
+﻿using HouseControl.Library;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Linq;
+
+namespace HouseControl.Editor
+{
+    public class EditorViewModel : INotifyPropertyChanged
+    {
+        private string filename = AppDomain.CurrentDomain.BaseDirectory + "ScheduleData";
+        public string FileName
+        {
+            get { return filename; }
+            set
+            {
+                if (filename == value) return;
+                filename = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private Schedule localSchedule;
+        public Schedule LocalSchedule
+        {
+            get { return localSchedule; }
+            set
+            {
+                if (localSchedule == value) return; 
+                localSchedule = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public EditorViewModel()
+        {
+            LocalSchedule = new Schedule(filename);
+        }
+
+        public void SaveSchedule()
+        {
+            LocalSchedule.SaveSchedule();
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
